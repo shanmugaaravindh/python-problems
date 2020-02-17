8193444dbace3410236d4dc50e639591c201e063
shanmugaaravindh <61138237+shanmugaaravindh@users.noreply.github.com>
2020-02-17 14:26:22 +0530

    Create pattern print.py

---
+++ pattern print.py
@@ -0,0 +1,15 @@
+a=int(input())
+for i in range(0,a):
+    for j in range(0,a):
+            print("*",end=" ")
+    print()
+'''
+sample input:
+5
+sample output:
+* * * * *
+* * * * * 
+* * * * *
+* * * * *
+* * * * *
+'''
